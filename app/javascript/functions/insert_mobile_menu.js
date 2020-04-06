ca0b48eb5b17528a16f5eb8715ea43ead1d90d51
francisco <franciscoantunesbarreto1997@gmail.com>
2020-04-06 14:13:21 +0100

    added overlay after burguer menu click

--- app/javascript/functions/insert_mobile_menu.js
+++ app/javascript/functions/insert_mobile_menu.js
@@ -1,16 +1,38 @@
+import disableScroll from 'disable-scroll';
+
 const insertMobileMenu = () => {
   const burguerMenu = document.querySelector('.burguer-menu');
   const lines = document.querySelector('.lines');
+  const mobileMenu = document.querySelector('.mobile-menu');
   let menuOpen = false;
+
+  const items = `<div class="mobile-menu-items">
+                    <a href="/">Projects</a>
+                    <a href="/articles">Articles</a>
+                    <a href="/contact">Contact</a>
+                    <a href="https://github.com/franciscobarreto1997"><i class="fab fa-github"></i></a>
+                  </div>`
 
   if (burguerMenu) {
     burguerMenu.addEventListener('click', (event) => {
       if (!menuOpen) {
         burguerMenu.classList.add('open');
         menuOpen = true;
+        mobileMenu.removeAttribute('hidden');
+        setTimeout(() => {
+          mobileMenu.classList.remove('opacity');
+        }, 10)
+        mobileMenu.insertAdjacentHTML('beforeend', items);
+        disableScroll.on();
       } else {
         burguerMenu.classList.remove('open');
         menuOpen = false;
+        mobileMenu.classList.add('opacity');
+        setTimeout(() => {
+          mobileMenu.setAttribute('hidden', '');
+        }, 250)
+        mobileMenu.innerHTML = "";
+        disableScroll.off();
       }
     })
   }
